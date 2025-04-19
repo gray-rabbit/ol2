057d00cb31eb4c3f573930a32e0f3df902089922
Sungjun Park <talksis@nate.com>
2025-04-19 02:28:54 +0000

    10869

---
+++ 1000.py
@@ -0,0 +1,8 @@
+# %%
+a, b =  input().split()
+a = int(a) # 문자열을 정수로 바꾼다! int
+b = int(b) # 문자열을 정수로 바꾼다!
+print(a+b)
+
+
+# %%
---
+++ 10869.py
@@ -0,0 +1,11 @@
+# %%
+a ,b = input().split()
+a = int(a)
+b = int(b)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)  #몫 구하는 연산자
+print(a%b)  #나머지를 구하는 연산자 
+# %%
